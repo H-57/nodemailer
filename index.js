a329eaa8de29f5927bb8830d6e94d4faa0c169a5
tarun <tarunkumar8059@gmail.com>
2023-11-22 15:20:06 +0530

    set view directory

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app=express();
 
 
 app.set('view engine',"ejs")
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 app.use(cookieParser());
