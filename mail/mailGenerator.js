bcba5322ea247c8191ced1ef371939b21999b89b
tarun <tarunkumar8059@gmail.com>
2023-12-13 16:23:38 +0530

    change header

--- mail/mailGenerator.js
+++ mail/mailGenerator.js
@@ -1,33 +1,53 @@
-let mail1={subject:``,message:``}
-let mail2={subject:``,message:``}
+let subject;
+let message; 
+
 
 
 
 export const mailGenerator = (name,jobId,emailtype) => {
 if(emailtype=="apply"){
-    mail1.subject=` Applying for the position of -${jobId} `
-    mail1.message=`Dear Sir/Madam,
+    subject=` Applying for the position of -${jobId} `
+  message=`Dear Sir/Madam,
 
-I hope you are well and reading my email. My name is ${name}, and I'm writing to express interest in the ${jobId} position.
+  I have attached my resume for the position of ${jobId}.
 
-The prospect of joining your team and assisting in the expansion and success of the business excites me. For your review, I've included my resume in this email, and if you require any more details, just let me know.
+  In my CV, I've listed information about my skills,educational background, and character traits.
 
-Thank you for considering my application. I look forward to the opportunity of discussing my qualifications further.
+  After looking over the job description, I am confident that I meet all the necessary requirements.
 
-Sincerely,
+  I've conducted considerable research on your company, found your path fascinating, and look forward to what the future holds. It would be an honour for me to help with that.
+  Please let me know whether you received this email. Contact me if you have any question.
+
+  I'm eager to learn what will happen next.
+  Sincerely,
+  
 
 ${name}`
+
     return {
-        subject:mail1.subject,
-        message:mail1.message
+        subject:subject,
+        message:message
     }
 }
+
 else if(emailtype=="refferal"){
-    mail2.subject=`${name} apply for job`
-    mail2.message=`${name} apply for job`
+    subject=`Exploring Exciting Opportunities: Seeking Your Guidance`
+    message=`I hope this message finds you in high spirits. 😊 I'm reaching out because I believe in the power of connections, and your name came up as someone who understands the value of seizing great opportunities.
+
+    I'm currently exploring new horizons in my career journey and have heard amazing things about your network. Your guidance or a referral would mean the world to me. I've attached my résumé for your reference.
+    
+    I understand your time is precious, and I assure you, a few minutes of your guidance can make a significant difference in my professional trajectory.
+    
+    Thank you so much for considering my request. Looking forward to the possibility of connecting with you and learning from your invaluable experience.
+    
+    Best regards,
+    
+    ${name}
+    
+    for -${jobId} `
     return {
-        subject:mail2.subject,
-        message:mail2.message
+        subject:subject,
+        message:message
     }
 }
    
